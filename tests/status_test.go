925476600b2508388b420f351de7306381a9200b
alexZaicev <alex.zaicef@gmail.com>
2022-10-10 21:18:34 +0100

    ft workflow

--- tests/status_test.go
+++ tests/status_test.go
@@ -2,7 +2,7 @@ package tests
 
 import (
 	"bytes"
-	"fmt"
+	"errors"
 	"strings"
 	"testing"
 
@@ -11,6 +11,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 
+	ftperrors "github.com/alexZaicev/go-ftp-client/internal/domain/errors"
 	"github.com/alexZaicev/go-ftp-client/internal/drivers/cli"
 	"github.com/alexZaicev/go-ftp-client/tests/utils"
 )
@@ -61,12 +62,9 @@ func (s *StatusTestSuite) Test_StatusTest_Happy() {
 
 	// assert
 	require.NoError(s.T(), err)
-
 	assert.Empty(s.T(), errBuffer.String())
 
 	result := outBuffer.String()
-	fmt.Println(result)
-
 	for _, eh := range []string{
 		"STATUS", "SYSTEM", "REMOTE ADDRESS", "LOGGED IN USER", "TLS ENABLED",
 	} {
@@ -80,91 +78,84 @@ func (s *StatusTestSuite) Test_StatusTest_Happy() {
 	}
 }
 
-// func (s *StatusTestSuite) Test_StatusTest_InvalidAddress() {
-//	// arrange
-//	outBuffer := bytes.NewBufferString("")
-//	errBuffer := bytes.NewBufferString("")
-//
-//	s.clientCMD.SetOut(outBuffer)
-//	s.clientCMD.SetErr(errBuffer)
-//	s.clientCMD.SetArgs([]string{
-//		"status",
-//		"-a",
-//		"not-valid-address:21",
-//		"-u",
-//		s.config.User,
-//		"-p",
-//		s.config.Password,
-//	})
-//
-//	// act
-//	err := s.clientCMD.Execute()
-//
-//	// assert
-//	assert.NotEmpty(s.T(), outBuffer.String())
-//	assert.NotEmpty(s.T(), errBuffer.String())
-//
-//	require.EqualError(s.T(), err, "an internal error occurred: failed to establish connection")
-//	assert.IsType(s.T(), ftperrors.InternalErrorType, err)
-//	assert.EqualError(s.T(), errors.Unwrap(err), "an internal error occurred: failed dial server on [not-valid-address:21] address")
-// }
-//
-// func (s *StatusTestSuite) Test_StatusTest_InvalidLogin() {
-//	testCases := []struct {
-//		name           string
-//		user           string
-//		pass           string
-//		expectedErrMsg string
-//	}{
-//		{
-//			name:           "invalid user",
-//			user:           "not-valid",
-//			pass:           s.config.Password,
-//			expectedErrMsg: "an internal error occurred: Login incorrect.",
-//		},
-//		{
-//			name:           "invalid pass",
-//			user:           s.config.User,
-//			pass:           "not-valid",
-//			expectedErrMsg: "an internal error occurred: failed to authenticate user",
-//		},
-//	}
-//
-//	for _, tc := range testCases {
-//		s.T().Run(tc.name, func(t *testing.T) {
-//			// arrange
-//			outBuffer := bytes.NewBufferString("")
-//			errBuffer := bytes.NewBufferString("")
-//
-//			s.clientCMD.SetOut(outBuffer)
-//			s.clientCMD.SetErr(errBuffer)
-//			s.clientCMD.SetArgs([]string{
-//				"status",
-//				"-a",
-//				s.config.Address,
-//				"-u",
-//				tc.user,
-//				"-p",
-//				tc.pass,
-//			})
-//
-//			server := mock_server.NewFtpServer(s.config)
-//			require.NoError(s.T(), server.Start(), "error starting mock FTP server")
-//
-//			// act
-//			err := s.clientCMD.Execute()
-//
-//			// assert
-//			// stop mock server before reading error channel
-//			server.Stop()
-//			require.NoError(s.T(), server.Error(), "error recorded by the server")
-//
-//			assert.NotEmpty(s.T(), outBuffer.String())
-//			assert.NotEmpty(s.T(), errBuffer.String())
-//
-//			require.EqualError(s.T(), err, "an internal error occurred: failed to authenticate with provided user account")
-//			assert.IsType(s.T(), ftperrors.InternalErrorType, err)
-//			assert.EqualError(s.T(), errors.Unwrap(err), tc.expectedErrMsg)
-//		})
-//	}
-// }
+func (s *StatusTestSuite) Test_StatusTest_InvalidAddress() {
+	// arrange
+	outBuffer := bytes.NewBufferString("")
+	errBuffer := bytes.NewBufferString("")
+
+	s.clientCMD.SetOut(outBuffer)
+	s.clientCMD.SetErr(errBuffer)
+	s.clientCMD.SetArgs([]string{
+		"status",
+		"-a",
+		"not-valid-address:21",
+		"-u",
+		s.config.User,
+		"-p",
+		s.config.Password,
+	})
+
+	// act
+	err := s.clientCMD.Execute()
+
+	// assert
+	assert.NotEmpty(s.T(), outBuffer.String())
+	assert.NotEmpty(s.T(), errBuffer.String())
+
+	require.EqualError(s.T(), err, "an internal error occurred: failed to establish connection")
+	assert.IsType(s.T(), ftperrors.InternalErrorType, err)
+	assert.EqualError(s.T(), errors.Unwrap(err), "an internal error occurred: failed dial server on [not-valid-address:21] address")
+}
+
+func (s *StatusTestSuite) Test_StatusTest_InvalidLogin() {
+	testCases := []struct {
+		name           string
+		user           string
+		pass           string
+		expectedErrMsg string
+	}{
+		{
+			name:           "invalid user",
+			user:           "not-valid",
+			pass:           s.config.Password,
+			expectedErrMsg: "an internal error occurred: failed to authenticate user",
+		},
+		{
+			name:           "invalid pass",
+			user:           s.config.User,
+			pass:           "not-valid",
+			expectedErrMsg: "an internal error occurred: failed to authenticate user",
+		},
+	}
+
+	for _, tc := range testCases {
+		s.T().Run(tc.name, func(t *testing.T) {
+			// arrange
+			outBuffer := bytes.NewBufferString("")
+			errBuffer := bytes.NewBufferString("")
+
+			s.clientCMD.SetOut(outBuffer)
+			s.clientCMD.SetErr(errBuffer)
+			s.clientCMD.SetArgs([]string{
+				"status",
+				"-a",
+				s.config.Address,
+				"-u",
+				tc.user,
+				"-p",
+				tc.pass,
+			})
+
+			// act
+			err := s.clientCMD.Execute()
+
+			// assert
+			assert.NotEmpty(s.T(), outBuffer.String())
+			assert.NotEmpty(s.T(), errBuffer.String())
+
+			require.EqualError(s.T(), err, "an internal error occurred: failed to authenticate with provided user account")
+			assert.IsType(s.T(), ftperrors.InternalErrorType, err)
+			assert.EqualError(s.T(), errors.Unwrap(err), tc.expectedErrMsg)
+		})
+	}
+}
